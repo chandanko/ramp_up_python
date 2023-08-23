be3f51408eff423a1aeaaa4d5824e4ecf8bd5d6c
chandanko <117801915+chandanko@users.noreply.github.com>
2023-08-23 16:46:15 +0530

    p3.py

--- p3.py
+++ p3.py
@@ -1,31 +1,44 @@
 import re
 import subprocess
 
-pattern= "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$"
+
+# pattern= "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$"
 # pattern="^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+def ip_val():
+    try:
+        pattern = "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$"
+        while True:
 
-while True:
-    User_input=input("Want to validate and ping Ip address !!! Type an option Yes or No -> ").lower()
-    if User_input=="yes":
-        Ip_address=input("Enter an ip address --> ")
+            Ip_address = input("Enter an ip address --> ")
 
-        data=re.search(pattern,Ip_address)
-        if data:
-            result=subprocess.run(['ping',Ip_address])
-            if result.returncode ==0:
-                print(f"The ping to {Ip_address} was successful")
-                with open("successful_Ips.txt", 'a') as file:
-                    file.write(Ip_address)
+            data = re.search(pattern, Ip_address)
+            if data:
+                result = subprocess.run(['ping', Ip_address])
+                if result.returncode == 0:
+                    print(f"The ping to {Ip_address} was successful")
+                    with open("successful_Ips.txt", 'a') as file:
+                        file.write(Ip_address + '\n')
+                else:
+                    print(f"The ping to {Ip_address} was unsuccessful")
+                    with open("Unsuccessful_Ips.txt", 'a') as file:
+                        file.write(Ip_address + '\n')
             else:
-                print(f"The ping to {Ip_address} was unsuccessful")
-                with open("Unsuccessful_Ips.txt", 'a') as file:
-                    file.write(Ip_address)
-        else:
-            pass
-    else:
-        break
+                pass
 
-# if data:
-#     print("valid")
-# else:
-#     print("invalid")
+            User_input = input("Want to validate and ping Ip address !!! Type an option Yes or No -> ").lower()
+            if User_input == "yes":
+                pass
+            else:
+                break
+
+
+    except:
+        print("Executed except block")
+
+
+ip_val()
+
+# 255=         25[0-5]
+# 200-249=    2[0-4][0-9]
+# 100-199=    1[0-9][0-9]
+# 0-99=       [1-9]?[0-9]
